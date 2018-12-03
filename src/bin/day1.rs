6a152b9d109f4590375871cf467648970bf313f4
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2018-12-03 08:21:04 -0500

    Put input parsing into one expression and changed from BTree to HashSet

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,14 +1,17 @@
-use std::collections::BTreeSet;
+use std::collections::HashSet;
 
 const INPUT: &'static str = include_str!("../../freq_deltas.txt");
 
 fn main() {
-    let input: Result<Vec<i64>, _> = INPUT.lines().map(str::parse).collect();
-    let input = input.unwrap();
+    let input: Vec<i64> = INPUT
+        .lines()
+        .map(str::parse)
+        .collect::<Result<_, _>>()
+        .unwrap();
 
     println!("Sum {}", input.iter().sum::<i64>());
 
-    let mut cache = BTreeSet::new();
+    let mut cache = HashSet::new();
     cache.insert(0);
 
     let result = input
